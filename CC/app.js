c07cf58ee1c1754deb5facdf00178e4886a6761a
Arfi23 <arfifaizilraushani@gmail.com>
2024-06-09 23:06:10 +0700

    create upload feature but still not working

--- CC/app.js
+++ CC/app.js
@@ -1,8 +1,13 @@
-const port = process.env.PORT || 3000;
 const express = require('express');
 const app = express();
 
+const multer = require('multer');
+const {Storage} = require('@google-cloud/storage');
+const storage = new Storage();
+const bucket = storage.bucket('kulitku-bucket');
+
 require('dotenv').config();
+const port = process.env.PORT || 3000;
 const bodyParser = require('body-parser');
 
 const userRouter = require('./src/users/users.router.js');
@@ -16,6 +21,45 @@ app.use('/', kulitRouter);
 
 // fitur scan
 
+// middleware multer
+const tempGCS = multer({
+    storage: multer.memoryStorage(),
+});
+
+app.post('/upload', tempGCS.single('file'), (req, res) => {
+    try {
+        // mendapatkan file dari request
+        const file = req.file;
+
+        // menyimpan gambar ke GCS
+        const gcsFileName = 'upload/' + file.originalname;
+        const gcsFile = bucket.file(gcsFileName);
+
+        // membuat stream untuk penulisan file ke GCS
+        const stream = gcsFile.createWriteStream({
+            metadata: {
+                contentType: file.mimetype,
+            },
+            resumable: false,
+        });
+
+        stream.on('error', (err) => {
+            console.error('Error terjadi saat upload ke GCS:', err);
+            res.status(500).json({ message: 'Kesalahan internal pada server' });
+        });
+
+        stream.on('finish', () => {
+            console.log('File di-upload ke GCS:', gcsFileName);
+            res.status(200).json({ message: 'File berhasil di-upload ke GCS' });
+        });
+
+        stream.end(file.buffer);
+
+    } catch (error) {
+        console.error('Terjadi kesalahan saat memproses request:', error);
+        res.status(500).json({ message: 'Kesalahan internal pada server' });
+    }
+})
 
 app.get('/' , (req , res)=>{
     res.send('halo server :)');
