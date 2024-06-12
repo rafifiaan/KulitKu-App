4cd7cc6c647fd068277b36cc7bd7e3714cce5e0d
Arfi23 <arfifaizilraushani@gmail.com>
2024-06-12 17:10:15 +0700

    Menambahkan autentikasi service account agar bisa upload dan mekanisme link GCS

--- CC/app.js
+++ CC/app.js
@@ -1,38 +1,46 @@
+// keperluan aplikasi web server
 const express = require('express');
 const app = express();
 
-const multer = require('multer');
-const {Storage} = require('@google-cloud/storage');
-const storage = new Storage();
-const bucket = storage.bucket('kulitku-bucket');
-
+// keperluan environment variable
 require('dotenv').config();
 const port = process.env.PORT || 3000;
 const bodyParser = require('body-parser');
 
+// keperluan upload file ke GCS
+const multer = require('multer');
+const {Storage} = require('@google-cloud/storage');
+const storage = new Storage({
+    keyFilename: process.env.GOOGLE_APPLICATION_CREDENTIALS,
+});
+const bucket = storage.bucket('kulitku-bucket');
+const tempGCS = multer({ // middleware multer
+    storage: multer.memoryStorage(), 
+});
+
+const crypto = require('crypto');
+
+// keperluan route
 const userRouter = require('./src/users/users.router.js');
 const kulitRouter = require('./src/kulit/kulit.route.js');
+// const predictRouter = require('./src/predict/predict.route.js');
+
+app.use('/', userRouter);
+app.use('/', kulitRouter);
+// app.use('/', predictRouter);
 
 app.use(express.json());
 app.use(bodyParser.json());
 
-app.use('/', userRouter);
-app.use('/', kulitRouter);
 
-// fitur scan
-
-// middleware multer
-const tempGCS = multer({
-    storage: multer.memoryStorage(),
-});
-
+// fitur upload ke GCS dan penyimpanan ke database
 app.post('/upload', tempGCS.single('file'), (req, res) => {
     try {
 
-        // Log request file
-        console.log('Received file:', req.file);
+        // log penerimaan file
+        console.log('File yang diterima:', req.file);
 
-        // Memeriksa apakah file ada
+        // memeriksa apakah file ada
         if (!req.file) {
             return res.status(400).json({ message: 'Tidak ada file yang diunggah' });
         }
@@ -44,7 +52,11 @@ app.post('/upload', tempGCS.single('file'), (req, res) => {
         const gcsFileName = 'upload/' + file.originalname;
         const gcsFile = bucket.file(gcsFileName);
 
+        // variabel penampung link object setelah upload GCS
+        let publicUrl;
+
         // membuat stream untuk penulisan file ke GCS
+        console.log('Membuat stream untuk upload ke GCS...');
         const stream = gcsFile.createWriteStream({
             metadata: {
                 contentType: file.mimetype,
@@ -57,13 +69,17 @@ app.post('/upload', tempGCS.single('file'), (req, res) => {
             res.status(500).json({ message: 'Kesalahan internal pada server' });
         });
 
-        stream.on('finish', () => {
+        stream.on('finish', async () => {
             console.log('File di-upload ke GCS:', gcsFileName);
+
+            // Link URL gambar yang di-upload
+            publicUrl = `https://storage.googleapis.com/${bucket.name}/${gcsFileName}`;
+            console.log(publicUrl);
             res.status(200).json({ message: 'File berhasil di-upload ke GCS' });
         });
 
+        console.log('Stream menyelesaikan proses...');
         stream.end(file.buffer);
-
     } catch (error) {
         console.error('Terjadi kesalahan saat memproses request:', error);
         res.status(500).json({ message: 'Kesalahan internal pada server' });
