1e2c5556b302dbd4b2a42075bd3a92fd5ee28700
Arfi23 <arfifaizilraushani@gmail.com>
2024-06-12 21:42:13 +0700

    Revise a variable naming

--- CC/config/databases.js
+++ CC/config/databases.js
@@ -3,7 +3,7 @@ const { createPool } = require('mysql')
 const pool = createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
+    password: process.env.DB_PASS,
     database: process.env.DB_NAME,
 })
 
