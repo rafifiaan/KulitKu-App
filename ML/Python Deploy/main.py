705a3ecb7c1156b1677c13191d053a22f0f3da87
lodaogos <115076652+lodaogos@users.noreply.github.com>
2024-06-13 18:58:12 +0700

    add main.py and model.h5

---
+++ ML/Python Deploy/main.py
@@ -0,0 +1,94 @@
+import os
+import tensorflow as tf
+from flask import Flask, request, jsonify
+from keras.models import load_model
+import numpy as np
+from tensorflow.keras.applications.mobilenet_v3 import preprocess_input
+from io import BytesIO
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__, static_url_path='', static_folder='static')
+
+# Load your .h5 model
+model = load_model('best_model.h5', compile=False)
+
+skin_disease = ["Acne", "Basal Cell Carcinoma", "Chicken Pox", "Eksim", "Melanoctyic Nevi",
+"Melanoma", "Normal Skin", "Psoriasis", "Ringworm", "Scabies", "Seborrheic Keratosis",
+"Shingles", "Urticaria Hives", "Warts Molluscum"]
+
+# Connect to databse
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqldb://root@localhost:3306/dbkulit'
+db = SQLAlchemy(app)
+
+
+class Penyakit(db.Model):
+    id_penyakit = db.Column(db.Integer, primary_key=True)
+    gambar_penyakit = db.Column(db.String(255), nullable=False)
+    nama_penyakit = db.Column(db.String(50), nullable=False)
+    definisi_penyakit = db.Column(db.Text)
+    penyebab_penyakit = db.Column(db.Text)
+    gejala_penyakit = db.Column(db.Text)
+    sinyal_ke_dokter = db.Column(db.Text)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        try:
+            file = request.files['file']
+            if file and file.filename != '':
+                img_path = BytesIO(file.read())
+            else:
+                respond = jsonify({'message': 'No file uploaded'})
+                respond.status_code = 400
+                return respond
+        except Exception:
+            respond = jsonify({'message': 'Error loading image file'})
+            respond.status_code = 400
+            return respond
+
+        img = tf.keras.utils.load_img(img_path, target_size=(224, 224))
+        x = tf.keras.utils.img_to_array(img)
+        x = np.expand_dims(x, axis=0).astype(np.float32)
+        x = preprocess_input(x)
+        images = np.vstack([x])
+
+        # Model predict
+        pred_disease = model.predict(images)
+        # Find the indexe prediction of the image
+        predicted_label_index = np.argmax(pred_disease)
+
+        # Print or log prediction information
+        print(f"Prediction: {predicted_label_index}")
+
+        # Check if the prediction is above a certain threshold
+        threshold = 0.2
+        if pred_disease[0][predicted_label_index] <= threshold:
+            respond = jsonify({'message': 'Penyakit Tidak Terdeteksi'})
+            respond.status_code = 400
+            return respond
+
+        # Prepare the response with prediction information
+        penyakit_data = Penyakit.query.order_by(Penyakit.id_penyakit).offset(predicted_label_index).first()
+        result = {
+            "predicted_label_index": int(predicted_label_index),
+            "confidence": float(pred_disease[0][predicted_label_index]),
+            "disease": str(skin_disease[predicted_label_index]),
+            "data": {
+                    "id_penyakit": penyakit_data.id_penyakit,
+                    "gambar_penyakit": penyakit_data.gambar_penyakit,
+                    "nama_penyakit": penyakit_data.nama_penyakit,
+                    "definisi_penyakit": penyakit_data.definisi_penyakit,
+                    "penyebab_penyakit": penyakit_data.penyebab_penyakit,
+                    "gejala_penyakit": penyakit_data.gejala_penyakit,
+                    "sinyal_ke_dokter": penyakit_data.sinyal_ke_dokter
+                }
+        }
+
+        respond = jsonify(result)
+        respond.status_code = 200
+        return respond
+
+    return 'OK'
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
