99df615128bbef900f7268b50b227efe32609c92
Andrian2992 <147039260+Andrian2992@users.noreply.github.com>
2024-06-21 15:29:23 +0700

    Add files via upload
    frontend application

---
+++ MD/KulitKu/app/src/main/java/com/example/kulitku/DaftarActivity.kt
@@ -0,0 +1,16 @@
+package com.example.kulitku
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class DaftarActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_daftar)
+        supportActionBar?.hide()
+    }
+}
